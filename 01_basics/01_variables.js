4acbcaa7cdf5aceecf02622c1b6d23bd552df334
pk-790324 <pappuyadav98199@gmail.com>
2024-11-16 01:44:27 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=90080
+
+// accountId =2333 not allowed
+
+let accountName="pappu yadav"   // let used in moder javascript
+var accountEmail ="pappu.9819@gmail.com"  // var is not prefer to used nowdays 
+let accountPassword="24939339"
+accountCity="Bhaktapur"  //not proper way to define variable
+let accountState;
+
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity,accountState])
+
